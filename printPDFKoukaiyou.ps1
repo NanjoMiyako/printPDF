0e44cad8a85a49b2a4a1051ce45d65b638dae7a7
NanjoMiyako <50001274+NanjoMiyako@users.noreply.github.com>
2022-09-18 16:51:18 +0900

    Add files via upload

---
+++ printPDFKoukaiyou.ps1
@@ -0,0 +1,28 @@
+﻿#SendKeysを使うため、System.Windows.Forms名前空間読込
+Add-Type -AssemblyName System.Windows.Forms
+
+#URLリストのパス
+$URLListPath = "C:\hogehoge\kyodoRyoriSyu1.txt" 
+##URLリストを読み込み
+$contents = Get-Content $URLListPath -Encoding UTF8
+
+Start-Sleep -s 10
+echo "Start"
+
+for($i=0; $i -lt $contents.Length; $i++){
+
+[System.Windows.Forms.SendKeys]::SendWait("^l")
+[System.Windows.Forms.SendKeys]::SendWait($contents[$i])
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Start-Sleep -s 3
+[System.Windows.Forms.SendKeys]::SendWait("^p")
+Start-Sleep -s 3
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Start-Sleep -s 3
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Start-Sleep -s 3
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+Start-Sleep -s 3
+
+}
+
---
+++ randomLinePickUpKoukaiyou.ps1
@@ -0,0 +1,8 @@
+﻿#URLリストのパス
+$URLListPath = "C:\hogehoge\kyodoRyoriSyu1.txt" 
+##URLリストを読み込み
+$contents = Get-Content $URLListPath -Encoding UTF8
+
+$rnd = Get-Random -Maximum ($contents.Length-1) -Minimum 1
+
+Start-Process $contents[$rnd]
